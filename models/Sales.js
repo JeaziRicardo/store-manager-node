6386fa4a0e00d310ecac543284fdff6b139628d8
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-22 20:46:37 -0300

    feat: cria a query para adicionar os dados da venda na tabela 'sales_products'

--- models/Sales.js
+++ models/Sales.js
@@ -1,12 +1,19 @@
 const connection = require('./connection');
 
-const sales = {
+const Sales = {
   insertSale: async () => {
-    const [insertId] = await connection.execute(
+    const [{ insertId }] = await connection.execute(
       'INSERT INTO StoreManager.sales (date) VALUES (NOW());',
     );
     return insertId;
   },
+
+  insertDataSale: async (saleId, productId, quantity) => {
+    await connection.execute(
+      'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?);',
+      [saleId, productId, quantity],
+    );
+  },
 };
 
-module.exports = sales;
+module.exports = Sales;
