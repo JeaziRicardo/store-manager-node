a517bbb9492ddab700bb019c61b8d763fd8a864b
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-23 19:09:04 -0300

    feat: cria a controller que exibe todas as vendas

--- controllers/sale.controller.js
+++ controllers/sale.controller.js
@@ -6,6 +6,12 @@ const saleController = {
 
     res.status(201).json(sale);
   },
+
+  allSales: async (_req, res) => {
+    const sales = await saleService.allSales();
+
+    res.status(200).json(sales);
+  },
 };
 
 module.exports = saleController;
