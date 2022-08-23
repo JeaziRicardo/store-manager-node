0b9f11a5da382124d5c40ac7fcfeebe698705848
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-23 19:17:38 -0300

    feat: cria o endpoint GET para exibir as vendas filtradas pelo id em '/sales/id'

--- routes/sales.route.js
+++ routes/sales.route.js
@@ -3,6 +3,8 @@ const saleController = require('../controllers/sale.controller');
 
 const saleRoute = Router();
 
+saleRoute.get('/:id', saleController.saleById);
+
 saleRoute.post('/', saleController.dataSale);
 saleRoute.get('/', saleController.allSales);
 
