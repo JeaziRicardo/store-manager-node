bef56dee0221f9de06ed4c3c30c7465384cef70b
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-24 10:09:17 -0300

    feat: cria o endpoint PUT para atualizar o pedido com a validação

--- routes/products.route.js
+++ routes/products.route.js
@@ -5,6 +5,7 @@ const validate = require('../middlewares/validateMiddleware');
 const prodRoute = Router();
 
 prodRoute.get('/:id', productController.productsById);
+prodRoute.put('/:id', validate.productName, productController.upProduct);
 
 prodRoute.get('/', productController.getProducts);
 prodRoute.post('/', validate.productName, productController.createProduct);
